dd0ac0ded6573a67c5627d23f2a159ec32e9ce00
Carlos Daniel <carlosporrasayala@gmail.com>
2024-11-15 01:23:58 -0500

    Validar de forma simple el formulario

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,16 +1,56 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { countries } from '../../data/countries';
 import styles from './Form.module.css';
+import { SearchType } from '../../types';
+import Alert from '../Alert/Alert';
 
 export default function Form() {
+  const [search, setSearch] = useState<SearchType>({
+    city: '',
+    country: '',
+  });
+
+  const [alert, setAlert] = useState<string>('');
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (Object.values(search).includes('')) {
+      setAlert('Todos los campos son obligatorios');
+      return;
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      {alert && <Alert>{alert}</Alert>}
       <div className={styles.field}>
         <label htmlFor='city'>Ciudad:</label>
-        <input id='city' name='city' type='text' placeholder='Ciudad' />
+        <input
+          id='city'
+          name='city'
+          type='text'
+          placeholder='Ciudad'
+          value={search.city}
+          onChange={(e) => handleChange(e)}
+        />
       </div>
       <div className={styles.field}>
         <label htmlFor='country'>País:</label>
-        <select id='country' name='country'>
+        <select
+          id='country'
+          name='country'
+          value={search.country}
+          onChange={handleChange}
+        >
           <option value=''>-- Seleccione un País --</option>
           {countries.map((country) => (
             <option key={country.code} value={country.code}>
