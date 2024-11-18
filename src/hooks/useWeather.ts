663346ac2244c5d73cd8903931861ad01e968716
Carlos Daniel <carlosporrasayala@gmail.com>
2024-11-18 02:47:39 -0500

    Tipar resultado de la API

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
+import { z } from 'zod';
 import { SearchType } from '../types';
+
+const Weather = z.object({
+  name: z.string(),
+  main: z.object({
+    temp: z.number(),
+    temp_min: z.number(),
+    temp_max: z.number(),
+  }),
+});
+
+type Weather = z.infer<typeof Weather>;
 
 export default function useWeather() {
   const fetchWeather = async (search: SearchType) => {
@@ -15,9 +27,22 @@ export default function useWeather() {
 
       const { data: weatherResult } = await axios.get(weatherUrl);
 
-      console.log(weatherResult);
+      const result = Weather.safeParse(weatherResult);
+
+      if (result.success) {
+        console.log(result.data);
+      }
     } catch (error) {
-      console.log(error);
+      if (axios.isAxiosError(error)) {
+        console.error(
+          'Error fetching data from API',
+          error.response?.data || error.message
+        );
+      } else if (error instanceof z.ZodError) {
+        console.error('Validation error', error.errors);
+      } else {
+        console.error('Unknown error', error);
+      }
     }
   };
 
